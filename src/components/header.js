d5fb82f49c669baf5c1b4ba6aff04dd186953c40
Ajay Patnaik <ajay-patnaik@users.noreply.github.com>
2018-09-06 14:32:00 -0400

    init

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './header.css'
+
+const Header = ({ siteTitle }) => (
+  <div
+    className="header-wrapper"
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <div>
+
+      </div>
+    </div>
+  </div>
+)
+
+export default Header
